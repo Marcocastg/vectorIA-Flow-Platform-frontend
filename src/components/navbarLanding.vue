ff88395d86d575fe8c5a8bd60737779ed9227bce
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-10-16 15:56:52 -0500

    update: logo

--- src/components/navbarLanding.vue
+++ src/components/navbarLanding.vue
@@ -10,7 +10,7 @@
 <header class="sticky top-0 z-50 w-full bg-background-light/80 dark:bg-background-dark/80 backdrop-blur-sm">
 <div class="container mx-auto flex items-center justify-between px-4 py-3 sm:px-6 lg:px-8">
 <div class="flex items-center gap-3">
-<img src="https://ik.imagekit.io/mcgonza/logo_ia_mejor11.png" alt="VectoriaFlow Logo" class="h-12 w-12">
+<img src="https://ik.imagekit.io/mcgonza/logo_ia_mejor11.png" alt="VectoriaFlow Logo" class="h-16 w-16">
 <path clip-rule="evenodd" d="M12.0799 24L4 19.2479L9.95537 8.75216L18.04 13.4961L18.0446 4H29.9554L29.96 13.4961L38.0446 8.75216L44 19.2479L35.92 24L44 28.7521L38.0446 39.2479L29.96 34.5039L29.9554 44H18.0446L18.04 34.5039L9.95537 39.2479L4 28.7521L12.0799 24Z" fill="currentColor" fill-rule="evenodd"></path>
 </img>
 <h2 class="text-xl font-bold text-gray-900 dark:text-white">VectoriaFlow</h2>
