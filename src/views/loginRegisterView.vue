6bd7ad174fd015648cb0bb1baa3b20ccb3ff731d
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-10-19 23:34:02 -0500

    update: login toast

--- src/views/loginRegisterView.vue
+++ src/views/loginRegisterView.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
-import { useToast } from 'vue-toastification';
+import { toast } from 'vue-toastification';
 
 // --- ESTADO ---
 // Esta variable controlará qué formulario se muestra: 'login' o 'register'.
